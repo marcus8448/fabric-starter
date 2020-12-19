68ce9675424b71d2c5a4d156a3f1dcd1c1527ff3
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2020-12-18 23:34:03 -0800

    feat: Initial commit

---
+++ src/main/java/io/github/marcus8448/mods/fabricStarter/FabricStarter.java
@@ -0,0 +1,11 @@
+package io.github.marcus8448.mods.fabricStarter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
+
