de83aa90c1f216e246a0fc6a32d313215d9a2bc8
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2021-02-12 11:27:40 -0800

    chore: update to use placeholders

---
+++ src/main/java/io/github/marcus8448/mods/modid/ModName.java
@@ -0,0 +1,11 @@
+package io.github.marcus8448.mods.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModName implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
+
